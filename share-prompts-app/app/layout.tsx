d58195a28585d8db3eed2568c09f0e9ff5823053
siriusoya <siriusoya@gmail.com>
2023-10-09 20:41:51 +0700

    project init

---
+++ share-prompts-app/app/layout.tsx
@@ -0,0 +1,24 @@
+import '*styles/globals.css'
+
+export const metadata = {
+    title: "Promptopia",
+    description: "Discover & Share AI Prompts",
+  };
+
+const RootLayout = () => {
+  return (
+    <html>
+        <body>
+            <div className='main'>
+                <div className='gradient' />
+            </div>
+
+            <main className='app'>
+                
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout
