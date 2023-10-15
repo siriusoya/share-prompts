3ca6c336b900e976bf6494689344da5c52e549fd
siriusoya <siriusoya@gmail.com>
2023-10-15 15:19:21 +0700

    google auth setup

--- share-prompts-app/app/layout.tsx
+++ share-prompts-app/app/layout.tsx
@@ -1,24 +1,33 @@
-import '*styles/globals.css'
+import "../styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
-    title: "Promptopia",
-    description: "Discover & Share AI Prompts",
-  };
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
 
-const RootLayout = () => {
-  return (
-    <html>
-        <body>
-            <div className='main'>
-                <div className='gradient' />
-            </div>
-
-            <main className='app'>
-                
-            </main>
-        </body>
-    </html>
-  )
+interface RootLayoutProps {
+  children: React.ReactNode;
 }
 
-export default RootLayout
+const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
+  return (
+    <html>
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
